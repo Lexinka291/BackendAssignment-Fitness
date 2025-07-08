4e38a509198a7c8815ba77f99670261c77ec650b
Lexi291 <alexandra291@gmail.com>
2025-07-08 10:38:57 +0200

    Adding pagination

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -1,28 +1,49 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import { Op } from "sequelize";
 
 import { models } from '../db'
 
 const router = Router()
 
 const {
-	Exercise,
-	Program
+    Exercise,
+    Program
 } = models
 
 export default () => {
-	router.get('/', async (_req: Request, res: Response, _next: NextFunction): Promise<any> => {
-		const exercises = await Exercise.findAll({
-			include: [{
-				model: Program,
-                as: 'program',
-			}]
-		})
+    router.get('/', async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+        try {
+            // Extract query params
+            // Pagination: /exercises?page=1&limit=10
+            const page = parseInt(req.query.page as string) || 1;
+            const limit = parseInt(req.query.limit as string) || 10;
 
-		return res.json({
-			data: exercises,
-			message: 'List of exercises'
-		})
-	})
 
-    return router
+            // Getting correct starting point
+            const nextPageOffset = (page - 1) * limit;
+
+            const { count, rows } = await Exercise.findAndCountAll({
+                offset: nextPageOffset,
+                limit,
+                include: [{
+                    model: Program,
+                    as: 'program',
+                }]
+            });
+
+            return res.json({
+                data: {
+                    total: count,
+                    page,
+                    pageSize: limit,
+                    exercises: rows,
+                },
+                message: 'List of exercises'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    return router;
 }
